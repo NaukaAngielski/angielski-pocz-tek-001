808268d3066cdc73124722e587f6a1f28b14b000
Nauka Angielski potem Chiński <118378454+NaukaAngielski@users.noreply.github.com>
2022-11-16 18:32:26 +0100

    Add files via upload

--- englishpoczatek001.jl
+++ englishpoczatek001.jl
@@ -13,4 +13,32 @@ FORSWORE - WYRZEKŁ SIĘ
 FORSWORE - WYRZEKŁ SIĘ 
 unforgivable - nieprzebaczalne 
 unforgivable - nieprzebaczalne 
-
+unforgivable - nieprzebaczalne 
+unforgivable - nieprzebaczalne 
+unforgivable - nieprzebaczalne 
+UNFORGIVABLE - NIEPRZEBACZALNE 
+EVIDENCE - DOWÓD, dane
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+DEMANDED - zażądał
+demanded - zażądał
+demanded - zażądał 
+demanded - zażądał
+demanded - zażądał 
+demanded - zażądał 
+demanded - zażądał
+demanded - zażadał
+demanded - zażądał
+forswore - wyrzekł sie 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+PASSAGE - PRZEJŚCIE 
+PASSAGE - PRZEJŚCIE 
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+evidence - dowód, dane 
+evidence - dowód, dane 
